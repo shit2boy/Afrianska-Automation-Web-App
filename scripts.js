36dc3b1d59260ca23f5f6c7e55e41f93ac6d6b7b
shit2boy <writeshittu@gmail.com>
2020-02-20 16:24:49 +0100

    updated

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const menu= ()=>{
+        const closebar = document.getElementById('menuItems');
+       if (closebar.style.display === 'none') {
+        closebar.style.display = 'block';
+       } else {
+        closebar.style.display = 'none'
+       };
+
+     }
